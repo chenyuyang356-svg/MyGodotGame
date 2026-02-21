f040b6bf85153b254744489a8302078a3dcf610c
txheeee <youtaochen074@gmail.com>
2026-02-21 21:29:12 +0800

    Reapply "Merge branch 'main' of https://github.com/chenyuyang356-svg/MyGodotGame"
    This reverts commit 5e48dc4b2c7d34067e9161e9608a206968d023f6.

--- the-range-of-justice/extension/src/unit_stats.h
+++ the-range-of-justice/extension/src/unit_stats.h
@@ -28,8 +28,11 @@ namespace godot {
 
         // --- 移动 ---
         float move_speed = 200.0f;
+        float acceleration = 1000.0f;      
         float turn_speed = 5.0f;
+        float turn_acceleration = 20.0f;
         float collision_radius = 10.0f;
+        float base_height = 0.0f;
         MoveType move_type = MOVE_GROUND;
 
         // --- 其他 ---
@@ -100,11 +103,20 @@ namespace godot {
         void set_move_speed(float p_value) { move_speed = p_value; }
         float get_move_speed() const { return move_speed; }
 
+        void set_acceleration(float p_val) { acceleration = p_val; }
+        float get_acceleration() const { return acceleration; }
+
         void set_turn_speed(float p_value) { turn_speed = p_value; }
         float get_turn_speed() const { return turn_speed; }
 
+        void set_turn_acceleration(float p_val) { turn_acceleration = p_val; }
+        float get_turn_acceleration() const { return turn_acceleration; }
+
         void set_collision_radius(float p_value) { collision_radius = p_value; }
         float get_collision_radius() const { return collision_radius; }
+
+        void set_base_height(float p_value) { base_height = p_value; }
+        float get_base_height() const { return base_height; }
 
         void set_move_type(MoveType p_value) { move_type = p_value; }
         MoveType get_move_type() const { return move_type; }
